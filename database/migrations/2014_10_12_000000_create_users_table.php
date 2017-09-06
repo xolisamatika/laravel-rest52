b70a000d033e3d745df47974946142bcad6de0e5
Xolisa Matika <xolisa.matika@bettech.com>
2017-09-06 15:17:51 +0200

    Corrected the first name column in the migration scripts.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('fisrt_name');
+            $table->string('first_name');
             $table->string('last_name');
             $table->string('phone');
             $table->string('status');
